12fbb6c4ba4c6bd42c472b827b309cd91bb47f05
DanielTisue <d.tisue@gmail.com>
2021-03-01 20:09:30 -0600

    Added query service. Implented receive events to both Comments and Posts service.

--- comments/index.js
+++ comments/index.js
@@ -16,7 +16,7 @@ app.get('/posts/:id/comments', (req, res) => {
 res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
 // This tells us the type of random 'id' we want to create. In this instance we are using 'hex'.
 const commentId = randomBytes(4).toString('hex');
 const { content } = req.body;
@@ -25,7 +25,9 @@ const comments= commentsByPostId[req.params.id] || [];
 
 comments.push({ id: commentId, content });
 
-axios.post('http://localhost:4005/events', {
+commentsByPostId[req.params.id] = comments;
+
+await axios.post('http://localhost:4005/events', {
   type: 'CommentCreated',
   data: {
     id: commentId, // taken from line 21 & 26 where commentId is established
@@ -35,11 +37,16 @@ axios.post('http://localhost:4005/events', {
 
 })
 
-commentsByPostId[req.params.id] = comments;
+
 
 res.status(201).send(comments);
 });
 
+app.post('/events', (req, res) => {
+  console.log('received event', req.body.type);
+
+  res.send({});
+});
 
 app.listen(4001, () => {
   console.log('Listening on 4001');
