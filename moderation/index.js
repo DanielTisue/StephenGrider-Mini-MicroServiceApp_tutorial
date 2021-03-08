5496576f89ff6f1d2717a778a3e1a75248d4b990
DanielTisue <d.tisue@gmail.com>
2021-03-07 19:00:33 -0600

    Finshed moderation service. Adjusted event-bus to emit event to Comment service. Updated comment service to post event with comment data.

--- moderation/index.js
+++ moderation/index.js
@@ -4,9 +4,29 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
 
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+    //if the status includes the word 'orange' then label status as rejected otherwise comment status is approved.
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    });
+  }
+
+  res.send({});
 });
+
+
+
 
 app.listen(4003, () => {
   console.log('Listening on 4003');
