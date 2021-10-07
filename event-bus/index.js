bc65df86cf2ee08bd00b6172935d2556c7d5d350
DanielTisue <d.tisue@gmail.com>
2021-10-06 20:06:40 -0500

    Added error handling.

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,13 +9,22 @@ app.use(express.json());
 app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
-  axios.post('http://localhost:4003/events', event);
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+      console.log(err.message);
+    });
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+      console.log(err.message);
+    });
+    
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+      console.log(err.message);
+    });
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+      console.log(err.message);
+    });
 //have to add error handling if the request fails.
   
-  res.send({ staus: 'OK' });
+  res.send({ status: 'OK' });
 });
 
 app.listen(4005, () => {
