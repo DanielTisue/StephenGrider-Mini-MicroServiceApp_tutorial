dd5ea583a8e3a8e73f02df0001171cb95b3071e8
DanielTisue <d.tisue@gmail.com>
2022-05-23 19:27:14 -0500

    Created comment moderation filter for comments.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,9 +3,24 @@ import React from 'react';
 import CommentCreate from './CommentCreate';
 
 export default ({ comments }) => {
-const renderedComments = comments.map(comment => {
-return <li key={comment.id}>{comment.content}</li>;
-});
+  const renderedComments = comments.map(comment => {
+      let content;
 
-return <ul>{renderedComments}</ul>;
+      //could also use switch statement may be better as it relates to industry standard
+
+      if (comment.status == 'approved') {
+        content = comment.content;
+      }
+
+      if (comment.status == 'pending') {
+        content = 'Comment is awaiting moderation.';
+      }
+
+      if (comment.status == 'rejected') {
+        content = 'Comment has not met approval requirements and has been rejected.';
+      }
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
 };
