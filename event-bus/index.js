0c38dfa9b6d58896aa13dca9753ad5d2ce5745fe
DanielTisue <d.tisue@gmail.com>
2022-05-23 19:29:34 -0500

    Added post method to post events to query service.

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,25 +6,33 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {
-      console.log(err.message);
-    });
-    axios.post('http://localhost:4001/events', event).catch((err) => {
-      console.log(err.message);
-    });
-    
-    axios.post('http://localhost:4002/events', event).catch((err) => {
-      console.log(err.message);
-    });
-    axios.post('http://localhost:4003/events', event).catch((err) => {
-      console.log(err.message);
-    });
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4003/events', event).catch((err) => {
+    console.log(err.message);
+  });
 //have to add error handling if the request fails.
   
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
