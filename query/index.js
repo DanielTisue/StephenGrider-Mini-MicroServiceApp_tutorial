5ccc34c9fcf8be35b64cea523a0a91698873bf9a
DanielTisue <d.tisue@gmail.com>
2022-05-23 19:31:10 -0500

    Added axios to query service. Added fallback helper function to handle events from event service.

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 //removed require body-parser due to notification to use the following below
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -19,14 +20,9 @@ const posts = {};
 //   }
 // }
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
- const { type, data } = req.body;
-
- if (type === 'PostCreated') {
+//Helper function handling events
+const handleEvent = (type, data) => {
+  if (type === 'PostCreated') {
   const { id, title } = data;
 
   posts[id] = { id, title, comments: [] };
@@ -50,12 +46,61 @@ app.post('/events', (req, res) => {
    comment.status = status;
    comment.content = content;
  }
+}
 
-  console.log(posts);
-
- res.send({});
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
-app.listen(4002, () => {
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+ //------------------------------
+ // Extracting the series of if statements into a separate helper function which can then be used through out the routes.
+//  if (type === 'PostCreated') {
+//   const { id, title } = data;
+
+//   posts[id] = { id, title, comments: [] };
+//  }
+
+//  if (type === 'CommentCreated') {
+//   const { id, content, postId, status } = data;
+
+//   const post = posts[postId];
+//   post.comments.push ({ id, content, status });
+//  }
+
+//  if ( type === 'CommentUpdated') {
+//    const { id, content, postId, status } = data;
+
+//    const post = posts[postId];
+//    const comment = post.comments.find(comment => {
+//      return comment.id === id;
+//    });
+// //-----------------------------------
+//    comment.status = status;
+//    comment.content = content;
+//  }
+
+  // console.log(posts);
+
+  handleEvent(type, data);
+
+  res.send({});
+});
+
+app.listen(4002, async () => {
   console.log('Listening on 4002');
+
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+ 
+    // could also use a forEach function
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
